5ee42cd3f1621305ed8a3c519d1fc1253edca56a
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-12-03 19:50:35 +0530

    Add data

--- home.js
+++ home.js
@@ -9,3 +9,157 @@ function button2() {
         document.getElementById('output-area').value = --x;
     }
 }
+
+
+// ----------------- Arrays -----------------
+
+let burgersArray = [];
+let beveragesArray = [];
+let dessertsArray = [];
+
+itemsList.forEach(element => {
+    if(element.type == "Burger"){
+        burgersArray.push(element);
+    }else if (element.type == "Beverage") {
+        beveragesArray.push(element);
+    }else if (element.type == "Dessert") {
+        dessertsArray.push(element);
+    }
+});
+
+// ----------------- Sorting Buttons -----------------
+
+let allItems = ``;
+let scrollableDiv = document.getElementById("scrollableDiv");
+
+function loadAllItems() {
+    allItems = ''; 
+
+    itemsList.forEach((element, index) => {
+        allItems += `
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3" onclick="addToOrder(${index})">
+                <div class="card">
+                    <img src="images/itemImages/${element.image}" class="card-img-top" alt="Item Image">
+                    <div class="card-body">
+                        <h5 class="card-title" id="itemName-${index}">${element.itemName}</h5>
+                        <p class="card-text" id="itemPrice-${index}">${element.price}</p>
+                    </div>
+                </div>
+            </div>`;
+    });
+    scrollableDiv.innerHTML = allItems;
+}
+
+function loadBurgers() {
+    allItems = ''; 
+
+    burgersArray.forEach((element, index) => {
+        allItems += `
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3" onclick="addToOrder(${index})">
+                <div class="card">
+                    <img src="images/itemImages/${element.image}" class="card-img-top" alt="Item Image">
+                    <div class="card-body">
+                        <h5 class="card-title" id="itemName-${index}">${element.itemName}</h5>
+                        <p class="card-text" id="itemPrice-${index}">${element.price}</p>
+                    </div>
+                </div>
+            </div>`;
+    });
+    scrollableDiv.innerHTML = allItems;
+}
+
+function loadBeverages() {
+    allItems = ''; 
+
+    beveragesArray.forEach((element, index) => {
+        allItems += `
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3" onclick="addToOrder(${index})">
+                <div class="card">
+                    <img src="images/itemImages/${element.image}" class="card-img-top" alt="Item Image">
+                    <div class="card-body">
+                        <h5 class="card-title" id="itemName-${index}">${element.itemName}</h5>
+                        <p class="card-text" id="itemPrice-${index}">${element.price}</p>
+                    </div>
+                </div>
+            </div>`;
+    });
+    scrollableDiv.innerHTML = allItems;
+}
+
+function loadDesserts() {
+    allItems = ''; 
+
+    dessertsArray.forEach((element, index) => {
+        allItems += `
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3" onclick="addToOrder(${index})">
+                <div class="card">
+                    <img src="images/itemImages/${element.image}" class="card-img-top" alt="Item Image">
+                    <div class="card-body">
+                        <h5 class="card-title" id="itemName-${index}">${element.itemName}</h5>
+                        <p class="card-text" id="itemPrice-${index}">${element.price}</p>
+                    </div>
+                </div>
+            </div>`;
+    });
+    scrollableDiv.innerHTML = allItems;
+}
+
+loadAllItems();
+
+
+let ordersArray = [];
+let ordersFlow = document.getElementById("ordersFlow");
+
+function addToOrder(index) {
+    let name = document.getElementById(`itemName-${index}`).innerText;
+    let price = document.getElementById(`itemPrice-${index}`).innerText;
+    
+    itemsList.forEach(element => {
+        if (name == element.itemName && !ordersArray.includes(element)) {
+            ordersArray.push(element);
+        }
+    });
+    renderOrders();
+}
+
+function renderOrders() {
+    let orderBody = ``;
+
+    ordersArray.forEach((element, index) => {
+    orderBody +=`
+        <div class="order-item" id="order-item-${element.id}">
+            <button class="btn-close" aria-label="Close" onclick="removeOrder('${element.id}')">&times;</button>
+            <div class="order-item-details">
+                <p class="item-name">${element.itemName}</p>
+                <div class="quantity-controls">
+                    <input type="button" class="quantity-btn" value="-" onclick="decreaseQuantity('${element.id}')"/>
+                    <input type="text" class="output-area-${element.id}" value="1" readonly />
+                    <input type="button" class="quantity-btn" value="+" onclick="increaseQuantity('${element.id}')"/>
+                </div>
+                <p class="price-${element.id}">${element.price}</p>
+            </div>
+        </div>`;
+    });
+
+    ordersFlow.innerHTML = orderBody;
+}
+
+function increaseQuantity(index) {
+    let qtyField = document.getElementById(`output-area-${index}`);
+    let currentQty = parseInt(qtyField.value);
+    qtyField.value = currentQty + 1;
+}
+
+function decreaseQuantity(index) {
+    let qtyField = document.getElementById(`output-area-${index}`);
+    let currentQty = parseInt(qtyField.value);
+    if (currentQty > 1) {
+        qtyField.value = currentQty - 1;
+    }
+}
+
+function removeOrder(index) {
+    ordersArray.splice(index, 1);
+    renderOrders(); 
+}
+
---
+++ itemsList.js
@@ -0,0 +1,211 @@
+const itemsList = [
+    {
+        id: 1,
+        itemName: "Green Chilli Burger",
+        price: 1350,
+        qty: 15,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Spicy burger with green chili flavor and crispy veggies.",
+        image: "greenChilliCheeseBurger.jpg"
+    },
+    {
+        id: 2,
+        itemName: "Pudding",
+        price: 500,
+        qty: 25,
+        type: "Dessert",
+        categoryCode: "DSRT",
+        availability: true,
+        description: "Soft and creamy pudding with a hint of vanilla.",
+        image: "pudding.jpg"
+    },
+    {
+        id: 3,
+        itemName: "Turkey Burger",
+        price: 1500,
+        qty: 10,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Premium turkey patty burger with fresh toppings.",
+        image: "turkeyBurger.jpg"
+    },
+    {
+        id: 4,
+        itemName: "Sprite",
+        price: 150,
+        qty: 50,
+        type: "Beverage",
+        categoryCode: "BEVR",
+        availability: true,
+        description: "Refreshing lemon-lime soda.",
+        image: "sprite.jpg"
+    },
+    {
+        id: 5,
+        itemName: "Ice Cream",
+        price: 600,
+        qty: 20,
+        type: "Dessert",
+        categoryCode: "DSRT",
+        availability: true,
+        description: "Creamy and delicious ice cream in various flavors.",
+        image: "iceCream.jpg"
+    },
+    {
+        id: 6,
+        itemName: "Chicken Burger",
+        price: 1200,
+        qty: 18,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Classic chicken burger with a crispy patty.",
+        image: "chickenBurger.jpg"
+    },
+    {
+        id: 7,
+        itemName: "Pepsi",
+        price: 180,
+        qty: 40,
+        type: "Beverage",
+        categoryCode: "BEVR",
+        availability: true,
+        description: "Crisp and refreshing cola soda.",
+        image: "pepsi.jpg"
+    },
+    {
+        id: 8,
+        itemName: "BBQ Burger",
+        price: 1400,
+        qty: 12,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Juicy BBQ-flavored burger with a smoky finish.",
+        image: "bbqBurger.jpg"
+    },
+    {
+        id: 9,
+        itemName: "Watalappam",
+        price: 700,
+        qty: 8,
+        type: "Dessert",
+        categoryCode: "DSRT",
+        availability: true,
+        description: "Traditional Sri Lankan dessert made with jaggery and coconut.",
+        image: "watalappam.jpg"
+    },
+    {
+        id: 10,
+        itemName: "Cheese Burger",
+        price: 1200,
+        qty: 20,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Cheesy burger with melted cheddar and fresh toppings.",
+        image: "cheeseBurger.jpg"
+    },
+    {
+        id: 11,
+        itemName: "Fruit Salad",
+        price: 700,
+        qty: 15,
+        type: "Dessert",
+        categoryCode: "DSRT",
+        availability: true,
+        description: "Fresh mixed fruit salad served chilled.",
+        image: "fruitSalad.jpg"
+    },
+    {
+        id: 12,
+        itemName: "Butter Burger",
+        price: 1400,
+        qty: 10,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Rich and buttery burger with caramelized onions.",
+        image: "butterBurger.jpg"
+    },
+    {
+        id: 13,
+        itemName: "Coca Cola",
+        price: 180,
+        qty: 35,
+        type: "Beverage",
+        categoryCode: "BEVR",
+        availability: true,
+        description: "Classic cola-flavored carbonated beverage.",
+        image: "cocaCola.jpg"
+    },
+    {
+        id: 14,
+        itemName: "Veggie Burger",
+        price: 1000,
+        qty: 12,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Healthy veggie patty loaded with flavor and fresh veggies.",
+        image: "veggieBurger.jpg"
+    },
+    {
+        id: 15,
+        itemName: "Mushroom Burger",
+        price: 1300,
+        qty: 8,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Earthy mushroom burger with a grilled patty.",
+        image: "mushroomBurger.jpg"
+    },
+    {
+        id: 16,
+        itemName: "Kik Cola",
+        price: 150,
+        qty: 40,
+        type: "Beverage",
+        categoryCode: "BEVR",
+        availability: true,
+        description: "Popular local cola-flavored soft drink.",
+        image: "kikCola.jpg"
+    },
+    {
+        id: 17,
+        itemName: "Burma Burger",
+        price: 1400,
+        qty: 15,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Flavorful burger with spices inspired by Burmese cuisine.",
+        image: "burmaBurger.jpg"
+    },
+    {
+        id: 18,
+        itemName: "Bison Burger",
+        price: 1600,
+        qty: 10,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Lean and savory bison meat patty in a toasted bun.",
+        image: "bisonBurger.jpg"
+    },
+    {
+        id: 19,
+        itemName: "Elk Burger",
+        price: 1800,
+        qty: 5,
+        type: "Burger",
+        categoryCode: "BURG",
+        availability: true,
+        description: "Premium elk patty burger with a unique flavor.",
+        image: "elkBurger.jpg"
+    }
+];
